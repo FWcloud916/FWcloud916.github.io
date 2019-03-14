071f7a63bc6c6eabdae88b18659f8448747bbf86
FWcloud916 <ree0916tw@gmail.com>
2019-03-15 01:07:53 +0800

    [add]set ci

---
+++ publish-to-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+cd ../
+mv .deploy_git/.git/ ./public/
+cd ./public
+git config user.name  "FWcloud916"
+git config user.email "ree0916tw@gmail.com" 
+# add commit timestamp
+git add .
+git commit -m "Travis CI Auto Builder at `date +"%Y-%m-%d %H:%M"`"
+git push --force --quiet "https://${Travis_gh_token}@${GH_REF}" master:master
